20d808aa936c0da0b883afc453cc60b8965abfc6
Rian Mendonça <rianmendonca@proton.me>
2024-03-13 11:07:56 -0300

    Selection Sort

--- src/algorithms/Algorithms.java
+++ src/algorithms/Algorithms.java
@@ -75,4 +75,21 @@ public class Algorithms {
         }
     }
 
+    // O(n^2)
+    public static void selectionSort(int[] array) {
+
+        for(int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[min] > array[j]) {
+                    min = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+
 }
