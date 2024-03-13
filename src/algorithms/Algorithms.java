d36abc501c5dea3155e613fb3188235c5203674a
Rian Mendonça <rianmendonca@proton.me>
2024-03-13 14:05:48 -0300

    Insertion Sort

--- src/algorithms/Algorithms.java
+++ src/algorithms/Algorithms.java
@@ -78,10 +78,10 @@ public class Algorithms {
     // O(n^2)
     public static void selectionSort(int[] array) {
 
-        for(int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int min = i;
-            for(int j = i + 1; j < array.length; j++) {
-                if(array[min] > array[j]) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
                     min = j;
                 }
             }
@@ -89,6 +89,24 @@ public class Algorithms {
             int temp = array[i];
             array[i] = array[min];
             array[min] = temp;
+        }
+    }
+
+    // O(n^2)
+    public static void insertionSort(int[] array) {
+
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+
+                array[j + 1] = array[j];
+                j--;
+
+            }
+
+            array[j + 1] = temp;
         }
     }
 
