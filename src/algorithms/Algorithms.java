9011bc323afb7a753a981afc15d1c96d2b849365
Rian Mendonça <rianmendonca@proton.me>
2024-03-14 15:18:15 -0300

    Quick Sort

--- src/algorithms/Algorithms.java
+++ src/algorithms/Algorithms.java
@@ -169,6 +169,40 @@ public class Algorithms {
         }
 
     }
+
+    public static void quickSort(int[] array, int start, int end) {
+
+        if (end <= start) return;
+
+        int pivot = partition(array, start, end);
+
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+
+    }
+
+    private static int partition(int[] array, int start, int end) {
+
+        int pivot = array[end];
+        int i = start - 1;
+
+        for (int j = start; j <= end; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+
+        return i;
+
+    }
+
 }
 
 
